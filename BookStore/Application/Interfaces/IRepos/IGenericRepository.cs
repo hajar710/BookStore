e42c41563f92ff7ef1b2bbb6ea287fd4c25812d4
Hajar Ismail <hajarismail2030@gmail.com>
2026-03-01 05:41:24 +0200

    add main files and Backages

---
+++ BookStore/Application/Interfaces/IRepos/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.IRepos
+{
+    internal class IGenericRepository
+    {
+    }
+}
---
+++ BookStore/Presentation/AutofacConfigurations.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces.IRepos;
+using Application.Interfaces.IServices;
+using Application.Services;
+using Autofac;
+using Infrastructure.Repositories;
+
+
+namespace Presentation
+{
+    public static class AutofacConfigurations
+    {
+        public static IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            // DBContext
+            builder.RegisterType<DBContext>().AsSelf().InstancePerLifetimeScope();
+
+            // Repositories
+            builder.RegisterType<AuthorRepository>().As<IAuthorRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<BookRepository>().As<IBookRepository>().InstancePerLifetimeScope();
+
+            // Services
+            builder.RegisterType<AuthorService>().As<IAuthorService>().InstancePerLifetimeScope();
+            builder.RegisterType<BookService>().As<IBookService>().InstancePerLifetimeScope();
+
+            return builder.Build();
+        }
+}
